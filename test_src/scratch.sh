55091b8b974a85a4a98dae45228bc031de4dce7e
Andrew E. Bruno <aebruno2@buffalo.edu>
2020-05-19 10:35:25 -0400

    Test both local/global scratch. Sleep to allow job to show in squeue

--- test_src/scratch.sh
+++ test_src/scratch.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-echo 'ccr sanitarium' > $TMPDIR/scratch-test.txt
+# Test writing to local scratch on the node
+echo "$TMPDIR - ccr sanitarium" > $TMPDIR/scratch-test.txt
 cat $TMPDIR/scratch-test.txt
+
+# Test writing to global scratch
+echo "$GLOBAL_SCRATCH - ccr sanitarium" > $GLOBAL_SCRATCH/${SLURM_JOB_ID}-scratch-test.txt
+cat $GLOBAL_SCRATCH/${SLURM_JOB_ID}-scratch-test.txt
+
+# Sleep for a bit show this job shows up in the queue
+sleep 30
