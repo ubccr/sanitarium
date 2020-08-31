d78d87c8fded6f44ec072522f31730f1ce877c99
Andrew E. Bruno <aebruno2@buffalo.edu>
2020-08-31 10:01:37 -0400

    Update global scratch test to use panasas volume

--- test_src/scratch.sh
+++ test_src/scratch.sh
@@ -8,8 +8,8 @@ echo "$TMPDIR - ccr sanitarium" > $TMPDIR/scratch-test.txt
 cat $TMPDIR/scratch-test.txt
 
 # Test writing to global scratch
-echo "$GLOBAL_SCRATCH - ccr sanitarium" > $GLOBAL_SCRATCH/${SLURM_JOB_ID}-scratch-test.txt
-cat $GLOBAL_SCRATCH/${SLURM_JOB_ID}-scratch-test.txt
+echo "$GLOBAL_SCRATCH - ccr sanitarium" > $GLOBAL_SCRATCH/grp-testsuite/${SLURM_JOB_ID}-scratch-test.txt
+cat $GLOBAL_SCRATCH/grp-testsuite/${SLURM_JOB_ID}-scratch-test.txt
 
 # Sleep for a bit show this job shows up in the queue
 sleep 30
