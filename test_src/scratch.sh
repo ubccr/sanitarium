e9c14185505449e3f8ce24bb0463daaa9c9279f8
Tony Kew <tkewtest@vortex-dev.int.ccr.buffalo.edu>
2023-10-18 09:36:50 -0400

    Update the scratch test to run as any user.
    Code searches for the first directory in the $GLOBAL_SCRATCH dir
    the current user can write to & uses that.
    
    Tested with multiple users.
    
    Tony

--- test_src/scratch.sh
+++ test_src/scratch.sh
@@ -3,13 +3,43 @@
 trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
 set -e
 
+#DIAGS
+#echo "TMPDIR=\"${TMPDIR}\""
+#echo "SLURM_JOB_ID=\"${SLURM_JOB_ID}\""
+#
+
+#
 # Test writing to local scratch on the node
-echo "$TMPDIR - ccr sanitarium" > $TMPDIR/scratch-test.txt
-cat $TMPDIR/scratch-test.txt
+#
+local_scratch_testfile="${TMPDIR}/${SLURM_JOB_ID}-scratch-test.txt"
+echo "Local Scratch test file \"${local_scratch_testfile}\""
+echo "${TMPDIR} - ccr sanitarium" > "${local_scratch_testfile}"
+cat "${local_scratch_testfile}"
+rm -f "${local_scratch_testfile}"
 
+#
 # Test writing to global scratch
-echo "$GLOBAL_SCRATCH - ccr sanitarium" > $GLOBAL_SCRATCH/grp-testsuite/${SLURM_JOB_ID}-scratch-test.txt
-cat $GLOBAL_SCRATCH/grp-testsuite/${SLURM_JOB_ID}-scratch-test.txt
+#
+# Get the group for the global scratch test directory path
+group="$(groups | sed 's/\s/\n/g' | grep ^grp- | while read group; do if [ -d ${GLOBAL_SCRATCH}/${group} ]; then   echo "${group}";   break; fi; done)"
+#
+if [ "${group}" != "" ]
+then
+  global_scratch_testfile="${GLOBAL_SCRATCH}/${group}/${SLURM_JOB_ID}-scratch-test.txt"
+  echo "Global Scratch test file \"${global_scratch_testfile}\""
+  echo "${GLOBAL_SCRATCH} - ccr sanitarium" > "${global_scratch_testfile}"
+  cat "${global_scratch_testfile}"
+  # cleanup
+  rm -f "${global_scratch_testfile}"
+else
+  echo "No Global scratch directory for the user ${LOGNAME}" >&2
+fi
 
-# Sleep for a bit show this job shows up in the queue
-sleep 30
+# Sleep for 30 seconds so this job shows up in the queue
+for i in {1..15}
+do
+  echo -n "."
+  sleep 2
+done
+echo
+
